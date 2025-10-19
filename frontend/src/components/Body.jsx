03ee762ec28737bf7238333df75f93dc004851ce
Mohammad Aamir <aamireverlasting786>
2025-10-20 02:25:25 +0530

    handled routing from inner route

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -2,90 +2,91 @@ import React, { useEffect, useState } from 'react'
 import NavBar from './NavBar'
 import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import {BASE_URL} from '../utils/constants'
+import { BASE_URL } from '../utils/constants'
 import { useDispatch, useSelector } from 'react-redux'
 import { addUser } from '../utils/userSlice'
 import BottomNavigation from './BottomNav'
 import { updateExpertStatus } from '../utils/expertDetailsSlice';
 import { Loader2 } from 'lucide-react'
 
-const Body = () =>{
+const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
-  const user = useSelector((store)=>store.user);
+  const user = useSelector((store) => store.user);
   const expertDetails = useSelector((store) => store.expertDetails)
-  console.log(expertDetails);
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
-  const hideTopNavBarPaths = ["/chat-list","/chat-box","/expert-dashboard"];
-  const shouldTopHideNavBar = hideTopNavBarPaths.some((path)=>
-    location.pathname.startsWith(path.replace(":id",""))
+  const hideTopNavBarPaths = ["/chat-list", "/chat-box", "/expert-dashboard"];
+  const shouldTopHideNavBar = hideTopNavBarPaths.some((path) =>
+    location.pathname.startsWith(path.replace(":id", ""))
   );
-  const hideBotNavBarPaths = ["/chat-box","/create-group-chat"];
-  const shouldBotHideNavBar = hideBotNavBarPaths.some((path)=>
-    location.pathname.startsWith(path.replace(":id",""))
+  const hideBotNavBarPaths = ["/chat-box", "/create-group-chat"];
+  const shouldBotHideNavBar = hideBotNavBarPaths.some((path) =>
+    location.pathname.startsWith(path.replace(":id", ""))
   );
-  const fetchUser = async ()=>{
-      
-    try{   
-      const res = await axios.get(BASE_URL+"/profile",{
-       withCredentials:true,
+  const fetchUser = async () => {
+
+    try {
+      const res = await axios.get(BASE_URL + "/profile", {
+        withCredentials: true,
       });
-    dispatch(addUser(res.data));
-    
-       }
-       catch (err) {
-        if (err?.response?.status === 401) {
-          navigate("/login");
-        } else {
-          console.log("Fetch user error", err);
-        }
-      } finally {
-        setLoading(false); 
-      }
-  }
-  const handleExpert = async ()=>{
-    try{
-     const getExpertDetails = await axios.get(BASE_URL + "/expert-details", { withCredentials: true });
-        dispatch(updateExpertStatus(getExpertDetails.data));
+      dispatch(addUser(res.data));
+
     }
-    catch(err){
+    catch (err) {
+      if (err?.response?.status === 401) {
+        navigate("/login");
+      } else {
+        console.log("Fetch user error", err);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+  const handleExpert = async () => {
+    try {
+      const getExpertDetails = await axios.get(BASE_URL + "/expert-details", { withCredentials: true });
+      dispatch(updateExpertStatus(getExpertDetails.data));
+    }
+    catch (err) {
       console.log(err)
 
     }
-   
+
   }
-  
-
- useEffect(() => {
-  if (user) {
-    handleExpert();
-  }
-}, [user]);
-
-useEffect(() => {
-  if (!user) {
-    fetchUser();
-  } else {
-    setLoading(false);
-  }
-}, []);
 
 
-  if (loading)  return  ( <div className="text-center py-8 flex flex-col items-center">
-  <Loader2 className="h-8 w-8 animate-spin text-blue-500" />
-  <p className="mt-3 text-gray-300 font-medium">Loading...</p>
-</div>
-)
- 
+  useEffect(() => {
+    if (user) {
+      handleExpert();
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (location.pathname === "/" && !user) {
+      setLoading(true);
+      fetchUser();
+    } else if (!user) {
+      fetchUser();
+    }
+  }, [location.pathname]);
+
+
+
+  if (loading) return (<div className="text-center py-8 flex flex-col items-center">
+    <Loader2 className="h-8 w-8 animate-spin text-blue-500" />
+    <p className="mt-3 text-gray-300 font-medium">Loading...</p>
+  </div>
+  )
+
   return (
     <div>
-     { !shouldTopHideNavBar && <NavBar/>}
-     <div className={`flex-1 ${!shouldBotHideNavBar?"pb-14":""}`}> 
+      {!shouldTopHideNavBar && <NavBar />}
+      <div className={`flex-1 ${!shouldBotHideNavBar ? "pb-14" : ""}`}>
         <Outlet />
       </div>
-   {!shouldBotHideNavBar && <BottomNavigation/>}
+      {!shouldBotHideNavBar && <BottomNavigation />}
     </div>
   )
 }
