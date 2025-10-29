a02e0287a4663df70e707e4caa37918e4dcd0b34
Mohammad Aamir <aamireverlasting786>
2025-10-29 17:15:50 +0530

    moved into userAuth folder

---
+++ frontend/src/components/userAuth/ForgotPassword.jsx
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../../utils/constants";
+import { useNavigate } from "react-router-dom";
+import { ArrowLeft } from "lucide-react"
+import toast from "react-hot-toast";
+
+const ForgotPassword = () => {
+  const [emailId, setEmailId] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+  const [otpSent, setOtpSent] = useState(false);
+  const [showToast, setShowToast] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (showToast) {
+      const timer = setTimeout(() => setShowToast(false), 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [showToast]);
+
+  const handleRequestOtp = async () => {
+    if (emailId.trim() === "") {
+      setError("Email ID is required");
+      return;
+    }
+    setLoading(true);
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/forget-password`,
+        { emailId },
+        { withCredentials: true }
+      );
+      setMessage(res.data.message);
+      toast.success(res.data.message);
+      setOtpSent(true);
+      setShowToast(true);
+      setError("");
+    } catch (err) {
+      setError(err?.response?.data?.message || "Something went wrong");
+      toast.error(err?.response?.data?.message || "Something went wrong");
+
+      setMessage("");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-dark-charcoal p-4 overflow-auto">
+      <div className="bg-gray-800 shadow-lg rounded-xl p-6 w-full max-w-md text-soft-white">
+        {/* Back Button */}
+        <button
+          onClick={() => navigate(-1)}
+          className="text-slate-400 hover:text-slate-100 hover:bg-slate-800 p-2 sm:p-3 rounded-md flex items-center text-sm sm:text-base transition-colors"
+        >
+          <ArrowLeft className="w-5 h-5 sm:w-6 sm:h-6 mr-2" />
+        </button>
+
+        <h2 className="text-2xl font-semibold text-center mb-6">Forgot Password</h2>
+
+        <label className="block mb-4">
+          <span className="text-sm text-soft-white">Email ID</span>
+          <input
+            type="email"
+            value={emailId}
+            onChange={(e) => setEmailId(e.target.value)}
+            placeholder="Enter your email"
+            className="input input-bordered w-full bg-gray-700 text-soft-white border-gray-600 focus:ring focus:ring-vibrant-clay mt-1"
+          />
+        </label>
+
+        {!otpSent && (
+          <button
+            onClick={handleRequestOtp}
+            className={`w-full py-2 mt-2 rounded-md font-medium ${loading
+              ? "bg-gray-600 cursor-not-allowed"
+              : "bg-vibrant-clay hover:bg-orange-600 transition"
+              }`}
+            disabled={loading}
+          >
+            {loading ? "Sending..." : "Send OTP"}
+          </button>
+        )}
+
+        {otpSent && (
+          <button
+            onClick={() => navigate("/reset-password", { state: { emailId } })}
+            className="w-full py-2 mt-4 bg-green-600 hover:bg-green-500 transition rounded-md font-medium"
+          >
+            Go to Reset Password
+          </button>
+        )}
+
+        {error && <p className="mt-4 text-sm text-red-500">{error}</p>}
+
+        {showToast && (
+          <div className="fixed top-5 left-1/2 transform -translate-x-1/2 bg-green-600 text-soft-white px-4 py-2 rounded-lg shadow-md">
+            {message}
+          </div>
+
+        )}
+
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;
---
+++ frontend/src/components/userAuth/Login.jsx
@@ -0,0 +1,220 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../../utils/constants';
+import { handleAxiosError } from '../../utils/handleAxiosError';
+
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(true);
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [passwordMatchError, setPasswordMatchError] = useState(false);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, [user, navigate]);
+  const handleLogin = async () => {
+    if (!emailId && !password) {
+      setError("Email and password are required");
+      return;
+    } else if (!emailId) {
+      setError("Email is required");
+      return;
+    } else if (!password) {
+      setError("Password is required");
+      return;
+    } else {
+      setError("");
+    }
+
+
+    setLoading(true);
+    try {
+      const res = await axios.post(BASE_URL + "/login", {
+        emailId,
+        password,
+      }, { withCredentials: true });
+      dispatch(addUser(res.data));
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      if (err?.response?.status === 401 || err?.response?.status === 400)
+        setError(err?.response?.data);
+      else {
+        handleAxiosError(err, {}, [400, 401], "login-error-toast");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (!password.startsWith(confirmPassword)) {
+      setPasswordMatchError(true);
+    }
+    else {
+      setPasswordMatchError(false);
+    }
+  }, [password, confirmPassword]);
+
+  const handleSignUp = async () => {
+    if (!firstName && !lastName && !emailId && !password && !confirmPassword) {
+      setError("All fields are required");
+      return;
+    } else if (!firstName) {
+      setError("First name is required");
+      return;
+    } else if (!lastName) {
+      setError("Last name is required");
+      return;
+    } else if (!emailId) {
+      setError("Email is required");
+      return;
+    } else if (!password) {
+      setError("Password is required");
+      return;
+    } else if (!confirmPassword) {
+      setError("Confirm password is required");
+      return;
+    } else {
+      setError("");
+    }
+
+
+    if (password !== confirmPassword) {
+      return;
+    }
+    setLoading(true);
+    try {
+      const res = await axios.post(BASE_URL + "/signup", {
+        firstName: firstName.trim(),
+        lastName,
+        emailId: emailId.trim(),
+        password,
+      }, { withCredentials: true });
+      dispatch(addUser(res.data));
+      navigate("/profile");
+    } catch (err) {
+      console.log(err);
+      if (err?.response?.status === 401 || err?.response?.status === 400)
+        setError(err?.response?.data);
+      else {
+        handleAxiosError(err, {}, [400, 401], "signup-error-toast");
+      }
+    } finally {
+      setLoading(false);
+
+    }
+  };
+
+  return (
+    <div className={` ${!isLoginForm ? "mt-9" : "mt-0"} flex justify-center items-center  min-h-screen bg-dark-charcoal p-4 overflow-auto `}>
+      <div className="card bg-gray-800 w-full max-w-md shadow-xl rounded-xl">
+        <div className="card-body p-6 max-h-[90vh] overflow-y-auto">
+          <h2 className="card-title text-2xl font-bold text-soft-white mb-6">{isLoginForm ? "Login" : "Sign Up"}</h2>
+          {!isLoginForm && (
+            <>
+              <label className="form-control w-full max-w-xs mb-4">
+                <span className="label-text text-soft-white mb-1">First Name</span>
+                <input
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  placeholder="Enter Your First Name"
+                  className="input input-bordered w-full bg-gray-700 text-soft-white"
+                />
+              </label>
+              <label className="form-control w-full max-w-xs mb-4">
+                <span className="label-text text-soft-white mb-1">Last Name</span>
+                <input
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  placeholder="Enter Your Last Name"
+                  className="input input-bordered w-full bg-gray-700 text-soft-white"
+                />
+              </label>
+            </>
+          )}
+          <label className="form-control w-full max-w-xs mb-4">
+            <span className="label-text text-soft-white mb-1">Email ID</span>
+            <input
+              type="text"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
+              placeholder="Enter your email"
+              className="input input-bordered w-full bg-gray-700 text-soft-white"
+            />
+          </label>
+          <label className="form-control w-full max-w-xs mb-4">
+            <span className="label-text text-soft-white mb-1">Password</span>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              className="input input-bordered w-full bg-gray-700 text-soft-white"
+            />
+          </label>
+          {!isLoginForm && (
+            <label className="form-control w-full max-w-xs mb-4">
+              <span className="label-text text-soft-white mb-1">Confirm Password</span>
+              <input
+                type="password"
+                value={confirmPassword}
+
+                onChange={(e) => { setConfirmPassword(e.target.value) }
+                }
+                placeholder="Confirm Password"
+                className="input input-bordered w-full bg-gray-700 text-soft-white"
+              />
+            </label>
+          )}
+          {passwordMatchError && (
+            <p className="text-red-500 text-sm mt-1">Passwords do not match</p>
+          )}
+          <p className="text-red-500 mb-4">{error}</p>
+          <div className="card-actions justify-center mt-6">
+            <button
+              onClick={isLoginForm ? handleLogin : handleSignUp}
+              className="btn bg-vibrant-clay hover:bg-orange-600 text-soft-white w-full"
+              disabled={loading || (!isLoginForm && passwordMatchError)}
+
+            >
+              {loading ? "Processing..." : isLoginForm ? "Login" : "Sign Up"}
+            </button>
+          </div>
+          <div className="card-actions justify-center mt-4">
+            {isLoginForm && (
+              <p
+                onClick={() => navigate("/forgot-password")}
+                className="hover:cursor-pointer text-electric-blue hover:underline"
+              >
+                Forgot Password?
+              </p>
+            )}
+          </div>
+          <div className="card-actions justify-center mt-4">
+            <p className="hover:cursor-pointer text-soft-white" onClick={() => setIsLoginForm((value) => !value)}>
+              {isLoginForm ? "Don't have an account? Sign up here." : "Already have an account? Log in here."}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Login;
