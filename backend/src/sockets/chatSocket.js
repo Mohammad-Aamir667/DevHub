c54e73e3a05f9144d7f15cbe035b73f1c16af03a
Mohammad Aamir <aamireverlasting786>
2025-12-17 01:59:08 +0530

    tested socket using Redis

--- backend/src/sockets/chatSocket.js
+++ backend/src/sockets/chatSocket.js
@@ -11,78 +11,81 @@ const chatSocket = (io, socket, socketUserMap) => {
     socket.leave(roomId);
   });
   socket.on("sendMessage", async (data) => {
-  try{
 
-    if(data.conversationId){
-      const conversation = await Conversation.findById(data.conversationId);
-    if (!conversation) return;
+
+    try {
+
+      if (data.conversationId) {
+        const conversation = await Conversation.findById(data.conversationId);
+        if (!conversation) return;
         const { fromUserId, conversationId, messageText, file, fileUrl, timestamp } = data;
-        if(messageText.trim() === "" && !file) return; // Ignore empty messages
-     const isParticipant = conversation.participants.some(
-        (id) => id.toString() === fromUserId._id.toString()
-      );
+        if (messageText.trim() === "" && !file) return; // Ignore empty messages
+        const isParticipant = conversation.participants.some(
+          (id) => id.toString() === fromUserId._id.toString()
+        );
 
-      if (!isParticipant) {
-        console.warn("Unauthorized message attempt by:", fromUserId);
-        return; 
-      }
-    const message = new Message({
-      fromUserId,
-      messageText,
-      conversation: conversationId,
-      file,
-      fileUrl,
-      timestamp: new Date(),
-    });
-    await message.save();
-    conversation.unreadCount += 1;
-    conversation.lastMessage = message._id;
-    await conversation.save();
-    socket.broadcast.to(conversationId).emit('receiveMessage', { messageText, fromUserId, file, fileUrl, timestamp });
-  }
-
-    else{
-       const generateRoomId = (fromUserId, toUserId) => {
-    return [fromUserId, toUserId].sort().join("_");
-  };
-      const { fromUserId, toUserId, messageText, file, fileUrl, timestamp } = data;
-      const message = new Message({
-        fromUserId,
-        messageText,
-        //may be a new conversation
-        conversation: null,
-        file,
-        fileUrl,
-        timestamp: new Date(),
-      });
-      await message.save();
-      let conversation = await Conversation.findOne({ participants: { $all: [fromUserId, toUserId] } });
-
-      if (!conversation) {
-        conversation = new Conversation({
-          participants: [fromUserId, toUserId].sort(),
-          lastMessage: message._id,
-          unreadCount: 1,
+        if (!isParticipant) {
+          console.warn("Unauthorized message attempt by:", fromUserId);
+          return;
+        }
+        const message = new Message({
+          fromUserId,
+          messageText,
+          conversation: conversationId,
+          file,
+          fileUrl,
+          timestamp: new Date(),
         });
-        await conversation.save();
-      } else {
+        await message.save();
         conversation.unreadCount += 1;
         conversation.lastMessage = message._id;
         await conversation.save();
+        socket.broadcast.to(conversationId).emit('receiveMessage', { messageText, fromUserId, file, fileUrl, timestamp });
       }
-    
-      message.conversation = conversation._id;
-      await message.save();
-       const roomId = generateRoomId(fromUserId._id,toUserId) 
-    
-      socket.broadcast.to(roomId).emit('receiveMessage', { messageText, fromUserId, file, fileUrl, timestamp });
+
+      else {
+        const generateRoomId = (fromUserId, toUserId) => {
+          return [fromUserId, toUserId].sort().join("_");
+        };
+        const { fromUserId, toUserId, messageText, file, fileUrl, timestamp } = data;
+        const message = new Message({
+          fromUserId,
+          messageText,
+          //may be a new conversation
+          conversation: null,
+          file,
+          fileUrl,
+          timestamp: new Date(),
+        });
+        await message.save();
+        let conversation = await Conversation.findOne({ participants: { $all: [fromUserId, toUserId] } });
+
+        if (!conversation) {
+          conversation = new Conversation({
+            participants: [fromUserId, toUserId].sort(),
+            lastMessage: message._id,
+            unreadCount: 1,
+          });
+          await conversation.save();
+        } else {
+          conversation.unreadCount += 1;
+          conversation.lastMessage = message._id;
+          await conversation.save();
+        }
+
+        message.conversation = conversation._id;
+        await message.save();
+        const roomId = generateRoomId(fromUserId._id, toUserId)
+
+
+        socket.broadcast.to(roomId).emit('receiveMessage', { messageText, fromUserId, file, fileUrl, timestamp });
+      }
+
+
+
+    } catch (err) {
+      console.error("Error handling message:", err);
     }
-
-  
-
-}catch(err){
-  console.error("Error handling message:", err);
-}
   });
 };
 
